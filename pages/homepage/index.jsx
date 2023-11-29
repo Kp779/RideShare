7a2b134b8aa4da9953b1291aae36fe2fe1ce73fb
patware887 <harshpatware1505@gmail.com>
2023-11-29 13:38:07 +0530

    protected route ui with json file

--- pages/homepage/index.jsx
+++ pages/homepage/index.jsx
@@ -1,13 +1,10 @@
 import React from "react";
-import HomeNavbar from "./navbar";
 import Herosection from "./herosection";
-import { Container } from "reactstrap";
 import Footer from "./footer";
 import About from "./about";
 const Home = () => {
     return(
         <>
-        <HomeNavbar/>
         <Herosection/>
         <About/>
         <Footer/>
--- pages/protected-route.js
+++ pages/protected-route.js
@@ -1,8 +1,13 @@
-import Dashboard from '@/dashboard/App';
+import { reactStrictMode } from '@/next.config';
 import withAuth from '@/utils/withAuth';
+import React from 'react';
+import Dashboard from './Dashboard';
 function ProtectedRoute() {
   return (
-    <Dashboard/>
+    <React.StrictMode>
+       {/* <Dashboard/> */}
+       <h1>harsh</h1>
+           </React.StrictMode>
   )
 };
 
